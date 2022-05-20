424070277717ed15c90831e4b629e789dafe247a
Sam Peterson <yoshiapolis@gmail.com>
2022-05-20 17:19:38 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Button.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Button {
+
+    private ButtonType type;
+    private int gamepad;
+
+    public Button(ButtonType type, int gamepad) {
+        this.type = type;
+        this.gamepad = gamepad;
+    }
+
+    public ButtonType getType() {
+        return this.type;
+    }
+
+    public int getGamepad() {
+        return this.gamepad;
+    }
+}
